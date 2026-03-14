5d0ec1968416aecc9f74b9efadddc5f5aecb8cf4
David <jdidkdkdmkdjdjdkdnndnde@gmail.com>
2026-03-14 19:51:31 +0700

    Add files via upload

---
+++ belajar -oop/latihan 3/index.php
@@ -0,0 +1,22 @@
+<?php
+include 'Mahasiswa.php';
+include 'MataKuliah.php';
+
+$mahasiswa_1 = new Mahasiswa();
+$matkul_1 = new MataKuliah();
+$matkul_2 = new MataKuliah();
+
+$matkul_1->setData('A11.12345', 'Pemrograman Berbasis Web');
+$matkul_2->setData('A11.12346', 'Pemrograman Web Lanjut');
+
+$mahasiswa_1->setData('A11.2024.15964', 'David Cornelius Saputra', [$matkul_1, $matkul_2], '2006-1-13');
+
+echo '<pre>';
+echo "Umur dihitung dengan method/function : <br>";
+print_r($mahasiswa_1->getData()[4]);
+echo "<br>";
+echo "<br>";
+echo "Nilai umur diberi langsung : <br>";
+$mahasiswa_1->umur = 100;
+print_r($mahasiswa_1->umur);
+echo '</pre>';
